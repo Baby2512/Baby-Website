83e897a0b0ab261011a39b759e626a201d919027
Baby2512 <babyelamurugan03@gmail.com>
2024-01-26 15:25:40 +0530

    one

--- src/App.js
+++ src/App.js
@@ -4,18 +4,26 @@ import Home from "./Pages/Home"
 import About from "./Pages/About"
 import Contact from "./Pages/Contact"
 import Service from "./Pages/Service"
+import Counter from "./Counter.js"
+import Effect from "./Effect.js"
+import Reducer from "./Reducer.js"
 export default function App(){
   return(
-    <BrowserRouter>
-      <Routes>
-         <Route path="/" element={<Layout/>}>
-         <Route index element={<Home/>}/>
-         <Route path="about" element={<About/>}/>
-         <Route path="contact" element={<Contact/>}/>
-         <Route path="service" element={<Service/>}/>
-          </Route>
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //      <Route path="/" element={<Layout/>}>
+    //      <Route index element={<Home/>}/>
+    //      <Route path="about" element={<About/>}/>
+    //      <Route path="contact" element={<Contact/>}/>
+    //      <Route path="service" element={<Service/>}/>
+    //       </Route>
+    //   </Routes>
+    // </BrowserRouter>
+    <div>
+      <Counter/>
+      <Effect/>
+      <Reducer/>
+    </div>
   )
 }
 // import React from 'react'
